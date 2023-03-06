b49a3852a8d645534eaf0d54c8f70dc8722e3539
Marcelo Sousa <601882+marcelosousa@users.noreply.github.com>
2023-03-06 23:15:32 +0000

    feat: add CollectionItem (correct)

---
+++ java/descartes/ColItem.java
@@ -0,0 +1,32 @@
+public class CollectionItem implements Comparator<CollectionItem> {
+   int getCardSet();
+   int getCardRarity();
+   int getCardId();
+   int cardType;
+
+
+   int compare (CollectionItem o1, CollectionItem o2) {
+     if (o1 == o2){
+       return 0;
+     }
+		 if (o1.getCardSet() > o2.getCardSet()) {
+		     return 1;
+		 }
+		 if (o1.getCardSet() < o2.getCardSet()) {
+		     return -1;
+		 }
+		 if (o1.getCardRarity() < o2.getCardRarity()) {
+		     return 1;
+		 }
+		 if (o1.getCardRarity() > o2.getCardRarity()) {
+		     return -1;
+		 }
+		 if (o1.getCardId() > o2.getCardId()) {
+		     return 1;
+		 }
+		 if (o1.getCardId() < o2.getCardId()) {
+		     return -1;
+		 }
+		 return o1.cardType - o2.cardType;  //watch out for overflow!
+	 }
+}
