cac3e2e9de62f586ccb65f2a4f169719e0e0853c
Marcelo Sousa <601882+marcelosousa@users.noreply.github.com>
2023-03-06 22:50:32 +0000

    feat: add AInt class

---
+++ java/descartes/ArrayInt-one.java
@@ -0,0 +1,29 @@
+public class AInt implements Comparator<AInt>{
+   int length;
+   int get(int pos);
+   
+   public int compare(AInt o1, AInt o2){
+     int index, aentry, bentry;
+     index = 0;
+     
+     while ((index < o1.length) && (index < o2.length)) {
+       aentry = o1.get(index);
+       bentry = o2.get(index);
+       if (aentry < bentry) {
+           return -1;
+       }
+       if (aentry > bentry) {
+           return 1;
+       }
+       index++;
+     }
+     
+     if (o1.length < o2.length) {
+       return -1;
+     }
+     if (o1.length > o2.length) {
+       return 1;
+     }
+     return 0;
+   }
+}
